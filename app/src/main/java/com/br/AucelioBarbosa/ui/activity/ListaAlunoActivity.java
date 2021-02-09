a849de87769c9b7664956042c7b7d3b4672fbf2d
Aucélio Barbosa <aucelio.barbosa@gmail.com>
2021-02-09 01:00:18 -0300

    refatoção do código atravez de estração.

--- app/src/main/java/com/br/AucelioBarbosa/ui/activity/ListaAlunoActivity.java
+++ app/src/main/java/com/br/AucelioBarbosa/ui/activity/ListaAlunoActivity.java
@@ -1,4 +1,5 @@
 package com.br.AucelioBarbosa.ui.activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,30 +13,50 @@ import com.br.AucelioBarbosa.R;
 import com.br.AucelioBarbosa.dao.AlunoDAO;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class ListaAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Lista de alunos";
+    private final AlunoDAO dao = new AlunoDAO();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Lista de alunos");
+
+        setTitle(TITULO_APPBAR);
         setContentView(R.layout.activity_lista_aluno);
 
-        AlunoDAO dao = new AlunoDAO();
+        configuracaoFabNovoAluno();
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        abreFromularioNovaActivity(dao, listaDeAlunos);
+    }
 
+    private void abreFromularioNovaActivity(AlunoDAO dao, ListView listaDeAlunos) {
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1,dao.todos()));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configuraLista();
+    }
+
+    private void configuracaoFabNovoAluno() {
         FloatingActionButton btnNovoAluno = findViewById(R.id.accessibility_action_aluno_fab_novo_aluno);
         btnNovoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ListaAlunoActivity.this,FormularioAlunoActivity.class));
+                startActivity(new Intent(ListaAlunoActivity.this,
+                        FormularioAlunoActivity.class));
             }
         });
+    }
 
+    private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
-
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,dao.todos()));
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dao.todos()));
     }
 }
